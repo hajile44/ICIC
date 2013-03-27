0f3d36df0ee8cae7ee219e5716cda9f80898563e
Sidney Hoover <sidneyhoover.us@gmail.com>
2013-03-27 11:31:06 -0700

    CIC_A2
    2nd version of CIC for Google Play

---
+++ Christian_I_Ching_2_mobileA/Resources/UZ1config.js
@@ -0,0 +1,16 @@
+AC.Config.set({
+    apiAdapter : "Web",
+    needFlashVersion : "9.0.0",
+    clientApi : "http://ship.applicationcraft.com/",
+    serverUrl : "http://ship.applicationcraft.com/service/",
+    serverUploadUrl : "http://ship.applicationcraft.com/service/",
+    uploadServerPath: "http://ship.applicationcraft.com/service/uploads/",
+    uploadServerDirectPath: "http://ship.applicationcraft.com/service/uploads/direct.php",
+    serverEventsUrl: 'http://notify.applicationcraft.com/events',
+    formatApi : "json",
+    serverThemes: "themes/",
+    localAppsPath: 'Apps/',
+    flashAdapterUrl : "wiziCore/api/server/adapter/WiziSendRequest.swf",
+    isDebugRequest : true,
+    googleApiKey: {"rows":[{"id":1,"data":["http://applicationcraft.com/","ABQIAAAACfMroKb5WzfdVTHbQi2OfRRGQFuqEDC-bwWLPZ60Ces-G56XXhTiOZYFBXlh0_EId2nXJCwq7iTNIg"],"ind":1}]}
+});
